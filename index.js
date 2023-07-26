4196f9538422055876beb14af14a2d5c17e17cd9
wigarbadri <wigarbadri@gmail.com>
2023-07-26 16:24:10 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(arguement){
+  if (arguement <= 400) {
+    return "This one is on me!"
+  } else if (arguement > 400 && arguement < 2000) {
+    return "That will be twenty bucks."
+  } else if (arguement > 2000 && arguement <2500) {
+    return "I will gladly take your thirty bucks."
+  } else if (arguement > 2500) {
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(chosenCity) {
+  const city = 'NYC'
+  // Create a variable called checkCity that has the string we want to return.
+  return chosenCity === city ? 'Ok, sounds good.' : 'No go.'
 }
+ternaryCheckCity()
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(amount){
+  switch(amount) {
+    case 'generous': 
+      return "Thank you so much."
+      break
+    case 'not as generous':
+      return "Thank you."
+      break
+    default:
+      return "Bye."
+  }
 }
